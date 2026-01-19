41a488c153361b834c07dbd470148cb9fea04e32
Yasmim <fiatcoskiy@gmail.com>
2026-01-18 21:57:29 -0300

    0Usuario.java
    Adicionando documentação

--- Biblioteca/Usuario.java
+++ Biblioteca/Usuario.java
@@ -1,10 +1,11 @@
 package biblioteca;
-public class Usuario {
+public class Usuario {        //encapsulamento
     private String nome;
-    
+    //Método construtor
     public Usuario(String nome){
         this.nome = nome;
     }
+    //Método get
     public String getNome(){
         return nome;
     }
